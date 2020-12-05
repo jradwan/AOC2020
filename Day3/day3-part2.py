31d91feee23a0f7adff063e7ff0783ff9ced73b0
Jeremy C. Radwan <jradwan@users.noreply.github.com>
2020-12-05 15:37:12 -0500

    code for day 3, part 2, corrected

--- Day3/day3-part2.py
+++ Day3/day3-part2.py
@@ -19,33 +19,33 @@ with open(file_name) as file_contents:
    right = int(input('Right: '))
    down  = int(input('Down:  '))
 
-   while curr_row < length:
+   while curr_row <= length:
+      wrapped = bool(False)
+      # advance right, wrap around if needed
+      if ((curr_col + right) > width):
+         wrapped = bool(True)
+         curr_col = (curr_col - width) + (right - 1) 
+      else:
+         curr_col += right
+
       # advance down and proceed if we haven't gone past the bottom of the map
       curr_row += down
       if (curr_row <= length):
          curr_line = line_content[curr_row].rstrip()
-         wrapped = bool(False)
-
-         # advance right, wrap around if needed
-         if ((curr_col + right) > width):
-            wrapped = bool(True)
-            curr_col = (curr_col - width) + (right - 1) 
-         else:
-            curr_col += right
 
          # check for a tree, keep count
          curr_char = curr_line[curr_col]      
          if (curr_char == '#'):
             tree_cnt += 1
 
-      # debugging block
-      print('\nLine', curr_row + 1, ': ', curr_line)
-      print('Col:  ', curr_col, end='')
-      if wrapped:
-         print(' (wrapped around)')
-      else:
-         print('')
-      print('Char: ', curr_char)
+         # debugging block
+         #print('\nLine', curr_row + 1, ': ', curr_line)
+         #print('Col:  ', curr_col + 1, end='')
+         #if wrapped:
+         #   print(' (wrapped around)')
+         #else:
+         #   print('')
+         #print('Char: ', curr_char)
 
    print('\nThe total # of trees encountered for slope ', right, '/', down, ' is: ', tree_cnt, '\n')
 
