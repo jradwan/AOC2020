8c20a1a1d918e8e1d6d9ba256f0fc1f2ef475348
Jeremy C. Radwan <jradwan@users.noreply.github.com>
2020-12-08 21:34:51 -0500

    code for day 7, part 1

--- Day7/day7-part1.py
+++ Day7/day7-part1.py
@@ -2,6 +2,29 @@
 # Day 7, Part 1
 # December 7, 2020
 
+def bag_recursion(bag_color):
+
+   global rules
+   global bags
+
+   #print('\nLooking for bag colors that can contain',bag_color,'bags:')
+   for curr_rule in rules:
+      if (curr_rule.find(bag_color) != -1):
+         outer_bag_color = curr_rule[:curr_rule.find(',')]
+         #print('Checking bag color:', outer_bag_color)
+         # ignore the bag color we're actually looking for, count the others
+         if (outer_bag_color != bag_color):
+            #print('Recursion ...')
+            bag_recursion(outer_bag_color)
+         else:
+            #print('Final outer bag: ', outer_bag_color)
+            bags.add(outer_bag_color)
+
+# end bag_recursion()   
+
+
+# *** main program begins here ***
+
 file_name = "day7-input.dat"
 
 with open(file_name) as file_contents:
@@ -13,7 +36,7 @@ with open(file_name) as file_contents:
    bag_cnt   = 0   
    rules     = set()
    bags      = set()
-   bag_type  = 'shiny gold'
+   bag_color = 'shiny gold'
    length    = len(line_content) - 1
 
    # load in and parse the rules
@@ -26,42 +49,21 @@ with open(file_name) as file_contents:
       curr_rule = curr_rule.replace(' bag', '')
       # replace 'contains'
       curr_rule = curr_rule.replace(' contain', ',')
-      print('Rule #', rule_cnt + 1, ': ', curr_rule)
+      #print('Rule #', rule_cnt + 1, ': ', curr_rule)
       # add the current, revised rule to the set
       rules.add(curr_rule)
 
       rule_cnt += 1
       curr_row += 1
 
-   #print(rules)
-   print('\nTotal # of rules loaded and parsed: ', rule_cnt) 
+   #print('\nTotal # of rules loaded and parsed: ', rule_cnt) 
 
-   # find the initial outer bag colors that contain the bag color we're interested in
-   print('\nOuter bag colors that can contain', bag_type, 'bags:')
-   for curr_rule in rules:
-      if (curr_rule.find(bag_type) != -1):
-         bag_clr = curr_rule[:curr_rule.find(',')]
-         # ignore the bag color we're actually looking for, count the others
-         if (bag_clr != bag_type):
-            print(bag_clr)
-            bags.add(bag_clr)
-            bag_cnt += 1
-            
-   print('Total: ', bag_cnt)
-   #print(bags)
+   # recurse through all the bag colors to determine which colors can 
+   # contain the bag_color we're interested in
+   bag_recursion(bag_color)
 
-   # now, for each of those outer bags, count the combinations of other bag colors
-   # that can contain THOSE bag colors 
-   for curr_bag in bags:
-      print('\nChecking which bags can contain a', curr_bag, 'bag ...')
-      for curr_rule in rules:
-         if (curr_rule.find(curr_bag) != -1):
-            bag_clr = curr_rule[:curr_rule.find(',')]
-            if (bag_clr != curr_bag):
-               print(bag_clr)
-               #bags.add(bag_clr)
-               bag_cnt += 1
-               #print(curr_rule)
+   # remove the bag color we're looking for from the final set 
+   bags.remove(bag_color)
 
-
-   print('\nTotal # of bag colors that could contain a',bag_type,':', bag_cnt, '\n')
+   #print('\nBag colors: ', bags)
+   print('\nTotal # of bag colors that can contain a',bag_color,'bag:', len(bags), '\n')
